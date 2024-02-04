a0de3a5cf4ee94a9aa6abd44f54106a1bec0594d
reimber <reimber@gmail.com>
2024-02-04 00:37:38 +0000

    added project folder name

--- main.lua
+++ main.lua
@@ -1,15 +1,17 @@
-function load()
-
+function love.load()
+    font = love.graphics.newFont('font/MontserratBlack.ttf',50) 
 end
 
 
-function update()
-
+function love.update(dt)
+    love.graphics.setFont(font)
 end
 
 
-function draw()
-
+function love.draw()
+    love.graphics.setBackgroundColor(1,1,1)
+    love.graphics.setColor(0,0,0) 
+    love.graphics.print('Hello World',50,50)
 end
 
 
